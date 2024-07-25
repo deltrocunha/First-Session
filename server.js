7758d76365cb17c45a136e577e1f034e64412bb2
deltrocunha <yazaldedelgado@gmail.com>
2024-07-25 13:42:28 +0100

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcrypt'
-/*import autencontroller from './controllers/autencontroller.js'*/
+import jwt from 'jsonwebtoken'
 
-/*const aut = new autencontroller()*/
+/*import routes from './routes/userroute.js'*/
+
+
 const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
@@ -19,8 +21,15 @@ app.get('/usuarios', async (req, res) => {
 
 app.post('/usuarios', async (req, res) => {
 
+    if (!req.body.name || !req.body.email || req.body.nif) {
+        res.status(400).send({
+            message: "Preencha todos os campos obrigatórios"
+        })
+
+    }
     req.body.senha = await bcrypt.hash(req.body.senha, 10)
     await prisma.user.create({
+
         data: {
             email: req.body.email,
             name: req.body.name,
@@ -59,16 +68,101 @@ app.put('/usuarios/:id', async (req, res) => {
 app.delete('/usuarios/:id', async (req, res) => {
 
     await prisma.user.delete({
-        where:{
+        where: {
             id: req.params.id
         }
-         
+
     })
     res.status(200).json("Usuário Eliminado")
     console.log('Usuário Eliminado');
 
 
 })
+
+app.post('/login', async (req, res) => {
+    const { email, senha } = req.body
+
+
+    const user = await prisma.user.findUnique({ where: { email } })
+    if (!user) {
+        return res.status(401).json({ error: 'Usuário não encontrado' })
+    }
+
+
+    const isValid = await bcrypt.compare(senha, user.senha);
+    if (!isValid) {
+        return res.status(401).json({ error: 'Senha incorreta' })
+    }
+
+
+    const token = jwt.sign({ userId: prisma.user.id }, process.env.SECRET_KEY, {
+        expiresIn: 200,
+    });
+
+
+    res.json({ token })
+});
+
+
+app.get('/autentica', authenticate, (req, res) => {
+    res.json({ message: 'Você está autenticado!' })
+});
+
+
+function authenticate(req, res, next) {
+    const token = req.headers(['x-access-token'] || req.headers['authorization'])
+    if (!token) {
+        return res.status(401).json({ error: 'Token não encontrado' })
+    }
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ error: 'Token inválido' })
+        }
+        req.id = decoded.user.id
+        next('autentica/prestador')
+    });
+}
+
+app.post('autentica/inicial', async (req, res) => {
+
+    const user = await prisma.user.findUnique({ where: { email } })
+
+    if (!authenticate) {
+        return res.authenticate
+    } else if (user.email === (user.tipousuario === "prestador")) {
+        res.status(200).send("Bem-Vindo" + user.name)
+        async function criarservico (req, res, next){
+            await prisma.servico.create({
+                data:{
+                    namedeservico: req.body.namedeservico,
+                    email: user.email,
+                    nif: user.nif,
+                    preco: req.body.preco
+
+                }
+
+            })
+        }
+
+    } else if(user.id === user.tipousuario === "cliente") {
+       
+        res.status(200).send("Bem-Vindo" + user.name)
+        async function contratarservico (req, res, next) {
+            await prisma.servico.create({
+                data:{
+                    namedeservico: req.body.namedeservico,
+                    email: user.email,
+                    nif: user.nif,
+                    preco: req.body.preco
+
+                }
+            })
+        }
+            }
+    
+        })
+
 
 
 
